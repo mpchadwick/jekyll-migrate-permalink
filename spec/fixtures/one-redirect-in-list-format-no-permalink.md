b5d3f247fdaf81785fb541344614c2c7aae1388c
Max Chadwick <mpchadwick@gmail.com>
2016-11-28 21:28:18 -0500

    Make sure append works

--- spec/fixtures/one-redirect-in-list-format-no-permalink.md
+++ spec/fixtures/one-redirect-in-list-format-no-permalink.md
@@ -1,5 +1,7 @@
 ---
 layout: post
+redirect_from:
+  - /current-redirect
 ---
 
 This post has no redirect and no redirects and no permalink.
--- spec/jekyll-migrate-permalink_spec.rb
+++ spec/jekyll-migrate-permalink_spec.rb
@@ -2,12 +2,22 @@ require 'spec_helper'
 
 describe(Jekyll::Migrate::Permalink) do
   it "adds a redirect to the front matter" do
-    content = source_dir("no-redirects-no-permalink.md")
-    url = "example.com/no-redirects-no-permalink"
+    content = File.read(source_dir("no-redirects-no-permalink.md"))
+    url = "/no-redirects-no-permalink"
     output = Jekyll::Commands::MigratePermalinks.process_content(content, url, "redirect")
 
     output =~ Jekyll::Document::YAML_FRONT_MATTER_REGEXP
     frontmatter = SafeYAML.load(Regexp.last_match(1))
     expect(frontmatter["redirect_from"]).to eql([url])
+  end
+
+  it "appends a redirect to the front matter if it already has one in list format" do
+    content = File.read(source_dir("one-redirect-in-list-format-no-permalink.md"))
+    url = "/one-redirect-in-list-format-no-permalink"
+    output = Jekyll::Commands::MigratePermalinks.process_content(content, url, "redirect")
+
+    output =~ Jekyll::Document::YAML_FRONT_MATTER_REGEXP
+    frontmatter = SafeYAML.load(Regexp.last_match(1))
+    expect(frontmatter["redirect_from"]).to eql(["/current-redirect", url])
   end
 end
